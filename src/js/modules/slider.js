478152f2550003a75cacab388e0dfe7fe459b200
Natasha Bandzeladze <na.bandzeladze@gmail.com>
2023-06-29 02:35:37 +0300

    feature: add main slider, add slider background

--- src/js/modules/slider.js
+++ src/js/modules/slider.js
@@ -1,5 +1,29 @@
-import Swiper, { Navigation, Pagination } from "swiper";
+import Swiper from "swiper/bundle";
 
 export function slider() {
-	
+    const sliderMain = new Swiper('.slider_main', {
+        freeMode: true,
+        centeredSlides: true,
+        mousewheel: true,
+        parallax: true,
+        breakpoints: {
+            0: {
+                slidesPerView: 2.5,
+                spaceBetween: 20,
+            },
+            680: {
+                slidesPerView: 3.5,
+                spaceBetween: 60,
+            }
+        }
+    });
+
+    const sliderBg = new Swiper('.slider_bg', {
+        centeredSlides: true,
+        parallax: true,
+        spaceBetween: 60,
+        slidesPerView: 3.5,
+    });
+
+    sliderMain.controller.control = sliderBg;
 }
